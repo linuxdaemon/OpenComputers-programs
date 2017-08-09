e917591858e7064843ca998af35d9981bb8bd676
linuxdaemon <linuxdaemon@snoonet.org>
2017-08-09 16:55:47 -0500

    Add check for duplicate enum elements

--- libs/enum.lua
+++ libs/enum.lua
@@ -3,6 +3,9 @@ return {
     local args = {...}
     local t = {}
     for k, v in ipairs(args) do
+      if t[v] then
+        return error("Duplicate enum entries")
+      end
       t[v] = k
     end
     return t
