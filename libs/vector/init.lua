50d2a35cf3f0fff14d55b4dc2238c8ceda230a7e
linuxdaemon <linuxdaemon@snoonet.org>
2018-10-24 20:19:51 -0500

    Update vector lib to use class lib

--- libs/vector/init.lua
+++ libs/vector/init.lua
@@ -1,72 +1,87 @@
+local class = require("class")
 local vector = {}
-local _vector = {
-  add = function( self, o )
-    return vector.new(
-      self.x + o.x,
-      self.y + o.y,
-      self.z + o.z
-    )
-  end,
-  sub = function( self, o )
-    return vector.new(
-      self.x - o.x,
-      self.y - o.y,
-      self.z - o.z
-    )
-  end,
-  mul = function( self, m )
-    return vector.new(
-      self.x * m,
-      self.y * m,
-      self.z * m
-    )
-  end,
-  dot = function( self, o )
-    return self.x*o.x + self.y*o.y + self.z*o.z
-  end,
-  cross = function( self, o )
-    return vector.new(
-      self.y*o.z - self.z*o.y,
-      self.z*o.x - self.x*o.z,
-      self.x*o.y - self.y*o.x
-    )
-  end,
-  length = function( self )
-    return math.sqrt( self.x*self.x + self.y*self.y + self.z*self.z )
-  end,
-  normalize = function( self )
-    return self:mul( 1 / self:length() )
-  end,
-  round = function( self, nTolerance )
-    nTolerance = nTolerance or 1.0
-    return vector.new(
-      math.floor( (self.x + (nTolerance * 0.5)) / nTolerance ) * nTolerance,
-      math.floor( (self.y + (nTolerance * 0.5)) / nTolerance ) * nTolerance,
-      math.floor( (self.z + (nTolerance * 0.5)) / nTolerance ) * nTolerance
-    )
-  end,
-  tostring = function( self )
-    return self.x..","..self.y..","..self.z
-  end,
-}
 
-local vmetatable = {
-  __index = _vector,
-  __add = _vector.add,
-  __sub = _vector.sub,
-  __mul = _vector.mul,
-  __unm = function( v ) return v:mul(-1) end,
-  __tostring = _vector.tostring,
-}
+local Vector = class()
 
-function vector.new( x, y, z )
-  local v = {
-    x = x or 0,
-    y = y or 0,
-    z = z or 0
-  }
-  setmetatable( v, vmetatable )
-  return v
+function Vector:_init(x, y, z)
+  self.x = x or 0
+  self.y = y or 0
+  self.z = z or 0
 end
+
+function Vector:add(other)
+  return Vector(
+    self.x + other.x,
+    self.y + other.y,
+    self.z + other.z
+  )
+end
+
+function Vector:sub(other)
+  return Vector(
+    self.x - other.x,
+    self.y - other.y,
+    self.z - other.z
+  )
+end
+
+function Vector:mul(scalar)
+  return Vector(
+    self.x * scalar,
+    self.y * scalar,
+    self.z * scalar
+  )
+end
+
+function Vector:dot(other)
+  local a = self.x * other.x
+  local b = self.y * other.y
+  local c = self.z * other.z
+  return a + b + c
+end
+
+function Vector:cross(other)
+  return Vector(
+      (self.y * other.z) - (self.z * other.y),
+      (self.z * other.x) - (self.x * other.z),
+      (self.x * other.y) - (self.y * other.x)
+    )
+end
+
+function Vector:length()
+  return math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+end
+
+function Vector:normalize()
+  return self:mul(1 / self:length())
+end
+
+function Vector:round(tolerance)
+  tolerance = tolerance or 1.0
+  local function _calc(n)
+    return math.floor((n + (tolerance * 0.5)) / tolerance) * tolerance
+  end
+  return Vector(
+    _calc(self.x),
+    _calc(self.y),
+    _calc(self.z)
+  )
+end
+
+function Vector:tostring()
+  return string.format("%s,%s,%s", self.x, self.y, self.z)
+end
+
+Vector.instance_meta.__add = Vector.add
+Vector.instance_meta.__sub = Vector.sub
+Vector.instance_meta.__mul = Vector.mul
+Vector.instance_meta.__unm = function(self) return self:mul(-1) end
+Vector.instance_meta.__tostring = Vector.tostring
+
+function vector.new(x, y, z)
+  return Vector(x, y, z)
+end
+
+vector.Vector = Vector
 
 return vector
